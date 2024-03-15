3e3b793e14a123fdd413d513a147f7647e9cfeb3
Patrick Sullivan <patrick.sullivan@hey.com>
2024-03-15 11:37:09 -0500

    simplified

--- src/lib.rs
+++ src/lib.rs
@@ -6,4 +6,4 @@ mod runner;
 pub use cache::GetCache;
 pub use job::Run;
 pub use processor::{McReceiver, MpSender, Processor, RequestSender, ResponseReceiver};
-pub use runner::{runner, Runner, RunnerError, RunnerSingle, RunnerThen};
+pub use runner::{runner, Runner, RunnerError};
--- src/runner.rs
+++ src/runner.rs
@@ -2,7 +2,6 @@ use crate::{
     job::Run,
     processor::{McReceiver, MpSender, RequestSender, ResponseReceiver},
 };
-use async_trait::async_trait;
 use std::{marker::PhantomData, time::Duration};
 use tokio::{
     task::{self, JoinError},
@@ -16,7 +15,7 @@ pub fn runner<J, JId, S, R, E>(
     request_sender: S,
     response_receiver: R,
     timeout: Duration,
-) -> RunnerSingle<J, JId, S, R, E>
+) -> Runner<J, JId, S, R, E>
 where
     J: Run,
     S: RequestSender<JobId = JId, Request = J::Request>,
@@ -24,7 +23,7 @@ where
     JId: Copy + Eq + for<'a> From<&'a J::Request>,
     E: From<<S as MpSender>::SenderError> + From<<R as McReceiver>::ReceiverError> + From<J::Error>,
 {
-    RunnerSingle {
+    Runner {
         request_sender,
         response_receiver,
         timeout,
@@ -34,117 +33,11 @@ where
     }
 }
 
-/// Runs a sequence of asynchronous jobs that depend on one another and whose
-/// input requests are passed to multiple-producer input channels and whose
-/// responses are published to multiple-consumer output channels.
-#[async_trait]
-pub trait Runner {
-    type J: Run;
-    type S: RequestSender<Request = <Self::J as Run>::Request>;
-    type R: ResponseReceiver<Response = <Self::J as Run>::Response>;
-    type E: From<<Self::S as MpSender>::SenderError>
-        + From<<Self::R as McReceiver>::ReceiverError>
-        + From<<Self::R as ResponseReceiver>::JobError>;
-
-    /// Context containing requests and responses to all runners that have
-    /// executed before this runner.
-    type OldCtx: Context;
-
-    /// Context containing requests and responses to all runners that have
-    /// executed including this runner.
-    type NewCtx: Context;
-
-    /// Runs the sequence of jobs.
-    async fn run(
-        &self,
-        req: <Self::J as Run>::Request,
-    ) -> Result<<Self::R as ResponseReceiver>::Response, RunnerError<Self::E>>;
-
-    async fn run_with_ctx(
-        &self,
-        req: <Self::J as Run>::Request,
-    ) -> Result<Self::NewCtx, RunnerError<Self::E>>;
-
-    /// Returns a new runner which executes this runner, converts the response
-    /// from this runner into a request for the [second] runner, and then
-    /// executes the [second] runner.
-    fn then<Second, F>(self, second: Second, f: F) -> RunnerThen<Self, Second, F>
-    where
-        Self: Sized,
-        Second: Runner,
-        F: Fn(Self::NewCtx) -> <Second::J as Run>::Request + Send + Sync,
-    {
-        RunnerThen {
-            first: self,
-            then: second,
-            f,
-        }
-    }
-}
-
-pub trait Context {
-    type Request;
-    type Response;
-    type OldCtx;
-
-    fn request(&self) -> Self::Request;
-    fn response(&self) -> Self::Response;
-    fn old_ctx(&self) -> Self::OldCtx;
-}
-
-pub struct EmptyCtx;
-
-impl Context for EmptyCtx {
-    type Request = ();
-    type Response = ();
-    type OldCtx = ();
-
-    fn request(&self) -> () {}
-
-    fn response(&self) -> () {}
-
-    fn old_ctx(&self) -> () {}
-}
-
-pub struct Ctx<Request, Response, OldCtx> {
-    pub request: Request,
-    pub response: Response,
-    pub old_ctx: OldCtx,
-}
-
-impl<Request, Response, OldCtx> Context for Ctx<Request, Response, OldCtx> {
-    type Request = Request;
-    type Response = Response;
-    type OldCtx = OldCtx;
-
-    fn request(&self) -> Request {
-        self.request
-    }
-
-    fn response(&self) -> Response {
-        self.response
-    }
-
-    fn old_ctx(&self) -> OldCtx {
-        self.old_ctx
-    }
-}
-
-impl<Request, Response, OldCtx> Ctx<Request, Response, OldCtx> {
-    pub fn new(request: Request, response: Response, old_ctx: OldCtx) -> Self {
-        Self {
-            request,
-            response,
-            old_ctx,
-        }
-    }
-}
-
 /// A runner for a single job.
 ///
 /// The runner will pass a given request to the [request_sender] and will wait
 /// for a corresponding response from the [response_receiver].
-pub struct RunnerSingle<J, JId, S, R, E>
+pub struct Runner<J, JId, S, R, E>
 where
     J: Run,
     S: RequestSender<JobId = JId, Request = J::Request>,
@@ -171,8 +64,7 @@ where
     _e: PhantomData<E>,
 }
 
-#[async_trait]
-impl<J, JId, S, R, E> Runner for RunnerSingle<J, JId, S, R, E>
+impl<J, JId, S, R, E> Runner<J, JId, S, R, E>
 where
     J: Run + Send + Sync + 'static,
     J::Request: Send,
@@ -190,17 +82,10 @@ where
         + Send
         + Sync,
 {
-    type J = J;
-    type S = S;
-    type R = R;
-    type E = E;
-    type OldCtx = EmptyCtx;
-    type NewCtx = Ctx<J::Request, J::Response, Self::OldCtx>;
-
-    async fn run(
+    pub async fn run(
         &self,
-        req: <Self::J as Run>::Request,
-    ) -> Result<<Self::R as ResponseReceiver>::Response, RunnerError<Self::E>> {
+        req: <J as Run>::Request,
+    ) -> Result<<R as ResponseReceiver>::Response, RunnerError<E>> {
         let req_job_id: JId = (&req).into();
         let receiver = self.response_receiver.clone();
 
@@ -225,76 +110,6 @@ where
             Ok(Err(e)) => Err(RunnerError::Join(e)),
             Err(_) => Err(RunnerError::Timeout),
         }
-    }
-
-    async fn run_with_ctx(
-        &self,
-        req: <Self::J as Run>::Request,
-    ) -> Result<Self::NewCtx, RunnerError<Self::E>> {
-        let resp = self.run(req).await?;
-        let new_ctx = Ctx::new(req, resp, EmptyCtx);
-        Ok(new_ctx)
-    }
-}
-
-/// A runner composed of a chain of runners.
-///
-/// This will execute the [first] runner and then map the response from the
-/// [first] runner into a request for the [second] runner.
-pub struct RunnerThen<First, Second, F>
-where
-    First: Runner,
-    Second: Runner,
-    F: Fn(<First as Runner>::NewCtx) -> <<Second as Runner>::J as Run>::Request + Send + Sync,
-{
-    pub(crate) first: First,
-    pub(crate) then: Second,
-    pub(crate) f: F,
-}
-
-#[async_trait]
-impl<First, Second, F> Runner for RunnerThen<First, Second, F>
-where
-    First: Runner + Send + Sync,
-    <First::J as Run>::Request: Clone + Send + Sync,
-    <First::J as Run>::Response: Send,
-    First::E: From<<Second::S as MpSender>::SenderError>
-        + From<<Second::R as McReceiver>::ReceiverError>
-        + From<<Second::R as ResponseReceiver>::JobError>,
-    Second: Runner<E = First::E> + Send + Sync,
-    <Second as Runner>::NewCtx: Context<Response = <<Second as Runner>::J as Run>::Response>,
-    <Second::J as Run>::Request: Send,
-    F: Fn(First::NewCtx) -> <Second::J as Run>::Request + Send + Sync,
-{
-    type J = (First::J, Second::J);
-    type S = First::S;
-    type R = Second::R;
-    type E = First::E;
-    type OldCtx = First::OldCtx;
-    type NewCtx = Ctx<
-        <<Second as Runner>::J as Run>::Request,
-        <<Second as Runner>::J as Run>::Response,
-        First::NewCtx,
-    >;
-
-    async fn run(
-        &self,
-        req: <Self::J as Run>::Request,
-    ) -> Result<<Self::R as ResponseReceiver>::Response, RunnerError<Self::E>> {
-        let resp_ctx = self.run_with_ctx(req).await?;
-        Ok(resp_ctx.response())
-    }
-
-    async fn run_with_ctx(
-        &self,
-        req: <Self::J as Run>::Request,
-    ) -> Result<Self::NewCtx, RunnerError<Self::E>> {
-        let first_resp_ctx = self.first.run_with_ctx(req.clone()).await?;
-        let second_req = (self.f)(first_resp_ctx);
-        let second_resp_ctx = self.then.run_with_ctx(second_req).await?;
-        let second_resp = second_resp_ctx.response();
-        let new_ctx: Self::NewCtx = Ctx::new(second_req, second_resp, first_resp_ctx);
-        Ok(new_ctx)
     }
 }
 
